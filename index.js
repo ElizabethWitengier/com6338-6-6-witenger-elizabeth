cda73cea8db20413091cb242dd765123bf225e9c
Elizabeth Witengier <ewitengier@ufl.edu>
2022-06-17 14:15:04 -0400

    final

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 var hamburgerBtn = document.querySelector('.hamburger-btn')
 var hamburgerMenu = document.querySelector('.hamburger-menu')
-var menu = document.getElementsByClassName('menu')
-var bodyE1 = document.querySelector('body')
+var section = document.querySelector('section')
+var banner = document.querySelector('.banner')
+var footer = document.querySelector('footer')
 
+function openMenu () {
+  hamburgerMenu.classList.toggle('show-menu')
+  hamburgerMenu.focus()
+  hamburgerBtn.getAttribute('aria-expanded', true)
+  hamburgerBtn.setAttribute('aria-expanded', false)
+}
 
-hamburgerBtn.addEventListener("click", () => {
-  
-  hamburgerMenu.classList.toggle("show-menu")
-})
+function closeMenu () {
+  hamburgerMenu.classList.remove('show-menu')
+  hamburgerBtn.focus()
+  hamburgerBtn.setAttribute('aria-expanded', false)
+}
+
+hamburgerBtn.onclick = openMenu
+section.onclick = closeMenu
+banner.onclick = closeMenu
+footer.onclick = closeMenu
+
+document.onkeyup = function(e) {
+  if (e.key === 'Escape')
+    closeMenu()
+}
