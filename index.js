87fd1605115108b1bccac2b4ceb1a3092fa361d0
Elizabeth Witengier <ewitengier@ufl.edu>
2022-06-17 12:27:04 -0400

    next round

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
+var hamburgerBtn = document.querySelector('.hamburger-btn')
+var hamburgerMenu = document.querySelector('.hamburger-menu')
 var menu = document.getElementsByClassName('menu')
-var hamMenu = document.getElementsByClassName('hamburger-menu')
 var bodyE1 = document.querySelector('body')
-var btn = document.getElementsByClassName('hamburger-btn')
 
-function launchMenu () {
-  hamMenu.classList.add('show-menu')
-}
 
-function launchMenu () {
-  hamMenu.classList.remove('show-menu')
-}
-
-btn.onclick = launchMenu
+hamburgerBtn.addEventListener("click", () => {
+  
+  hamburgerMenu.classList.toggle("show-menu")
+})
